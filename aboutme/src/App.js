99597b17e2ffe9a278895ebec4a06d0b2ce4c04e
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-11-21 15:39:23 +0200

    modified app.js

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Life from "./components/Life";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-black bg-white body-font">
+      <Navbar />
+      <About />
+      <Life/>
+      <Skills/>
+
+      <Projects/>
+      
+   
+    </main>
   );
 }
-
-export default App;
